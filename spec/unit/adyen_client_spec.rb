fb8aaa3cdb54244e2ff454cdf62e3eb220d84046
Martin Wöginger <martin.woeginger@gmail.com>
2018-07-10 15:07:25 +0200

    fix specs, renew vcr files

--- spec/unit/adyen_client_spec.rb
+++ spec/unit/adyen_client_spec.rb
@@ -3,9 +3,9 @@ require_relative "../spec_helper.rb"
 
 describe AdyenClient, vcr: { per_group: true } do
   # These encrypted card strings are taken form the client_side_encryption.html example in the root of the project directory
-  let(:encrypted_card) { "adyenjs_0_1_15$hAOv0nb0eX7hmXpvT5dK/obqbUjMaOIvJGA64m0cmn6VTZDBGco2EvY5/2SPXrty23kBQzZsUlO+KF8BR9kXTi4p2+Jky8RDywJqamkb5Y/6oSl7oU4p9WMN5APYjOfDseBT2GkibdYADSPGWatrKAYrn2t0S9qZkGriP9IxLhlwq9D8N/n9AWeOe4p0Aidr0NDm6KDxJZY7N67fSl55+Mu0jsiXE6fbQ03Mdtx8UNhMVHcANS8XH3OOwZk9dZMlkIOJDi7OgKbE0ZfqABh/VfhFzsJpZHKD508tCUB/90bY917A9mbIWgKSnxdYeSBh7T0+2QCGFBllR1vh4NgfAA==$wu1r67pMH3Z3zKVpbCXksyUkRrOlMeeFI9Fpz2G2df4Ad+gGD1+Gmzu0gD3H/F3B3BGwkF9Yqb2LJmyNRBLu2EkdoHBmb81cSTBpwnxRQ086aedugXUBp7Z+ZDt5eP6LhiJfBTN1RO56EDqxYUvIZ6hDBv/U272ZFPbVrLBAaz/RYbK/W4S8zK8pZ5UZq8g7Oo7qoumsdendhH2PfYpPm4rD/xFeMoxOwuYgVCqXIAsBKvJiuhDWdveUhCKC08uf8hrGtozP/rdl9vOIozjmA7NjZaYSatd5nD9p99sQMR/qHzAhi/tA9qchEqr85PbCDjWCrHqgy0Od3CFo/nmMFRHQFKb/qys=" }
-  let(:encrypted_card_wrong_cvc) { "adyenjs_0_1_15$lY7ZOXDCOw9oVuWTlGfuKdped5Hpf3nC/Lop79mZJ1VJqbx1Bl2+tf/BD1A2E0lled4NDpvZwpPfy9nux1MOZncgH4LP1aiWwYg1VcWBUBm1k1tN3gx7ZLUqDkuXHOyYfculgx1h/9R9hzwu4CQsMycLnnbjlWSAvGUchaZ4Ii6pmgTITvEzPK1N4vEaezRX9nSKKWbcTfA2jiDrqhPVvgr/aG4/e74TdVxcL4aWdeQFTLjQTeUB3fCn/SkJbKfwKlpC80IDGD9xrw2nWV6I+wB+bZ6vmSt2qoupTzeh9IbTOgFPtYboNjZFKaTL19tGf8UMagvR+o6FCmSVgsQSMQ==$/dSz2IDqi7XkEkYCktRZuUDZV51dbpOuyqXTNDDxUjBKYzKAm0knujDTxPVIoMhoRNJuN0jdeBUCLhl2R2J1kED1z9grgYJZqRoDUjaBJ33g1YP8Go8pi7rOBzOVkX1+TnH5/5CdB8S6kRH9bjOQRRMbfgOhyAFmIqQ6lF8WiLamYjLY59wwaHq0+QYQVkBjKHaYjW6pPGr97iEnayOg72oeMnuDj59yKXZf2/sovtXKu4S7t5pEVwP+G19vwoERbaEXKd+j7aWIPVXbjS1Xs0Sn7/c3B/X5HclNOioeddJ+RJbiRos3yC3eLBlxe/PVgW//v0wsB4TbpPwVYyvWjRTetvyQ8tKFiVfayMckOFGd+ws9gRQPSjqlxVStl5n40yO1e2UP4g==" }
-  let(:encrypted_card_wrong_exp) { "adyenjs_0_1_15$FEkA9DPTolK/s4PgIsW01NLX5z9otU4/n/evEqKJ5U7dPkZqHqLVXLwCHMmHr26XD+6N6H2S+etOKnaBgfKEkgckgcBPuXBTDYuHwSPa0dHudY7MpLTrlSEPgvy5VpT1WP74tixB5bxjovDULN1dVGrlw9LssNfRBj9laxTevrUAF/1GUDoeBSu45SUFNzZjkFUjDQan4XJlaAb5vexeLqiYIAW050MoW6iSxG0yM4GWMMPFvwfGVJEnYZJn3mhg7/uLw7y4Wyu7//sVVL4crIGvprwe8UNfsYBN3HzDqZwf1TdrOEn+TuyfqYWZbjN8U4oMK/Zq7q2cwP7a51A1bw==$GZe8zeZzVdi9VJDQdo940MO17kAF9/XE1iepfxghPW0TtWmdH7WktjlunA9RlW9CvNvpVgetJFJUB8PQd3fCl2GtcCO0Jx/gnhdL5RoAn5hAOCitB77ojP+qE9Dq1ZyfNkRUcs2b0D3IHz0W6IBvRwv3E7M9NCy2Sjskwqkyg8kY30pcggdp225+UDK9Xm4oHevu82C3A9WtXDCFjQ9S/5WEGvNAKIslAsBDgIOvE/lYQWcVOQ+6Dkr685A+dUD/lKd5q6a6fWRZo/fxQ4EyKzfrw61HA0UgsjZdZoqfbLk8JdtgLQKUrMup1RXQW5/MK5YXS6HIUiUx8QQZO8q9z4CpJpZZYrvyw2Se12EFhYAdfOBVvBiKdSLzaAocQysRbjWMiTSvMg==" }
+  let(:encrypted_card) { "adyenjs_0_1_15$NZcG1Vlfm6+9wygEzXFEjTCQ1go1RophriLC3DvvGKX5HH+3tI4q/xhZ9j6THxEHgbDrQoQFSjkxVJvteCkExFgnwKjRCJKH8M9AfOj4abyO/HQkJ+wR9rnVPxAeBHvHkkcYN0vJ4BxqqE+MX1cJjTvj15B/hKc6K1RNqNLg3h1aQas55GVxsYDT9pZM+ywMMY/rmhw0JcsbfJL17ui1fmwQCIMT0uOvWtWSP+OVYXoLYjlzEszWieIdip9ZCEhFIaK6dgsoPWwTu67mmWU+7EjwDDMmjMrzctcf5Q4Ofkx066bWULiYtAZjvkrdh67X5Ff13pVG0+gXMy5EaxJQGg==$lmXl2HmmBshGyG2Vs1NxQnJB/YGIwfNfvYjTkQUXxDkJOSkTAPc/cmmpJTgaNbwC7jGN/3qHfwGHt20IvipVpi9Q2ZIz0JOuQ2KPQtD3GG4xOTy1+KqJFFl8hiD055MoLxY8saeR51YybmVGK8xLwwow4Dce7InkD5x7rNXOrwQV6KErR0ZLzE2vU6ulKOg156XhSXaI3wjPJXRdI47+JaVvaulmz6drPZXaXXeEA7w0qfgDCNqD0FoyyxNV5h7g5qiRl0shmXGggCNWzPzdhPUQ0+WgOYcGGaxsZyEVXloRNjhU2rK9YnM4Xw04pekPFfNjvMcMpDmXAf8nQ4xXZnmwq7oAG2KQLmL4Bw5JxX4PC65kyVc=" }
+  let(:encrypted_card_wrong_cvc) { "adyenjs_0_1_15$h+rrBTrmDZSaaMXHPp08TH+1vz0Be/aS67H/NromtRwaReG4V8PDJG/xfHCVlHo6lZ63BhiZtzGV99rIuFB0tcdDVofGAuoEHoXFAlyaRb6aV8zt3ckNt9CPmeAaWNoRCACPPMba2I013Dut3Gn3KwDeBz08Wue/zUr4TQEIM84pprsJhKe2vkvcy0CLMuVkFCwxICYLehLIHVTN0hqJ/fZcq+cn+SW9DbZzIy+Ked5++UAuAnuVmvMVx2AjogswwG5TilIBdZGRHIJdkFzRrs+hl3h2vOkpfTUuYC38BUwUeaKcz3YpR3VypH8pL/MI47h2nwfCAIW1ZedD+0W9xQ==$I760jRxGuDXWUQHAUvh5psvn1VnC1/KsHbRbW8jRqu4DOMv/rtQGGdb7J3sl9h+tCIdad9/kpIoyt8TzRHk+3zcZjCygKL8iQ7EN97TGrCPOyytRW2qEngR6wB2H/Qxc1+bF0B+FswFzuJLgfZJZ1Uy92oKL4a+ZOt1WMvYv9ppwjP1kPSplxhWbUIDvvFfaoBHa61aFkb3BMu0yshTjATw4kkJlfKBdru3IotQrimRZsGFCcUJV/JPbJLjbb83qRqtpub+oax7+GM9ZLw1wBOz2744y7784sk0m30EivV/dxQUfFBnkD0GGXyz0+vdcphy60DyzH0h2rbWDdMt1V1v1AVSNlygIOL+d3Cri8tYpf7kjXUlko/wB0sgjecv1Ux/CrlI9XQ==" }
+  let(:encrypted_card_wrong_exp) { "adyenjs_0_1_15$ejfUxXsas1eqSSCPpZgs5AftI7VMJm9GvgXkCUa1mzuM9ZIZghLO/wYvP18eyOht/ukzR/DewYdlG9NGuk6jJewyzjJEDOY+vtbovMY90z2LfQlGDRCIXqvgjfLopSrXw22MGYiUgGvqNcZAO5CIkOucLXeObYKmEq97w/+qEwx7AJMIJQtOA/BDLdwEm23lLeWoHXs8CDE7aDGi4OLlrxp/lPfnfUbmPHNynlxx1cNwmqqTGSOq+zAuQr6KLSo5DGN483UsyjfXJtt2sgi2/CkPv3ZE8zHlWkx5Hrgt8/FKsvmU7NDv+YMu9w8uSML7OQEUkC9EixVHdpEp701whw==$THHVc01/56Mus1aJ5Vpb2RC1A1cmAe4RGJ7voYYS9D3oyXPez7okCC6nxT0ovLvsnySLOH1+LBiA8KZjkpogoulevyquICIMrbngIAv5Z2D8+QnrJ13XChvXJ7dtD4fW1o88WLZerxwReObCsGsizx3Fw0rhu8RowFy22ftDUHc2D4D2IAm9hXgt8PQmQx4ehCCUjXTVF8yEhPuHpA10xnbc3xRRKZrsGwwGi7Fl90bgVPT+dpVO1v0nlWM5Gr5Ji07Bmz5N1s5X4MJHs/stNe5ePzRDqI2dlkqOrY6cnDcARwxnT9TCfb3hVGsfYxoNgKOTBtyunhDOTfrhoMr8QxcxdxfPcH5VWSpMildt5BSfE3Hbuw4c3jVGHJrlTzx91LWMVwrr6w==" }
   let(:encrypted_card_garbled) { encrypted_card.gsub(/[abc]/, "x") }
   let(:shopper) { Hash[email: "john@doe.com", ip: "127.0.0.1", reference: "john"] }
   let(:client) { AdyenClient.new }
@@ -27,7 +27,7 @@ describe AdyenClient, vcr: { per_group: true } do
 
       it "should include the psp_reference in the response" do
         # taken from spec/cassettes/AdyenClient/verify/successful.yml
-        assert_equal "7914607123875349", response.psp_reference # taken from vcr cassette
+        assert_equal "8815312278659725", response.psp_reference # taken from vcr cassette
       end
     end
 
@@ -54,7 +54,7 @@ describe AdyenClient, vcr: { per_group: true } do
       end
 
       it "should include the psp_reference in the response" do
-        assert_equal "8514607123884459", response.psp_reference # taken from vcr cassette
+        assert_equal "8835312278663525", response.psp_reference # taken from vcr cassette
       end
     end
 
@@ -93,7 +93,7 @@ describe AdyenClient, vcr: { per_group: true } do
       end
 
       it "should include the psp_reference in the response" do
-        assert_equal "8514607123874435", response.psp_reference # taken from vcr cassette
+        assert_equal "8815312278689953", response.psp_reference # taken from vcr cassette
       end
     end
   end
@@ -121,7 +121,7 @@ describe AdyenClient, vcr: { per_group: true } do
       end
 
       it "should include the psp_reference in the response" do
-        assert_equal "8814607123882153", response.psp_reference # taken from vcr cassette
+        assert_equal "8825312278673617", response.psp_reference # taken from vcr cassette
       end
     end
   end
